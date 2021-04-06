b8652d6ed1b96e9978841b33824de88f420e5c63
Eduh Guarieiro <eduguarieiro@gmail.com>
2021-04-06 19:40:20 -0300

    Create the card-box.js component

---
+++ js/components/card-box.js
@@ -0,0 +1,38 @@
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".card-box.-one").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".card-box.-one .main-title").addClass("-white");
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".card-box.-one .main-title").removeClass("-white");
+      });
+});
+
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".card-box.-two").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".card-box.-two .main-title").addClass("-white");
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".card-box.-two .main-title").removeClass("-white");
+      });
+});
+
+var $JQuery2 = jQuery.noConflict()
+$JQuery2(function() {
+   $JQuery2(".card-box.-three").hover(
+      // função que adiciona a classe
+      function(){
+         $JQuery2(".card-box.-three .main-title").addClass("-white");
+      },
+      // função que remove a classe
+      function(){
+         $JQuery2(".card-box.-three .main-title").removeClass("-white");
+      });
+});
